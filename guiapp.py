62ff58c46858460b4aa502623423ec8ff1595c45
Nayan Thulkar <nayanthulkar1@gmail.com>
2020-10-02 02:52:15 +0530

    Added a reset button

--- guiapp.py
+++ guiapp.py
@@ -1,6 +1,6 @@
-# guiapp.py
 import pygame
 import time
+
 pygame.font.init()
 
 
@@ -37,7 +37,7 @@ class Grid:
             self.cubes[row][col].set(val)
             self.update_model()
 
-            if valid(self.model, val, (row,col)) and self.solve():
+            if valid(self.model, val, (row, col)) and self.solve():
                 return True
             else:
                 self.cubes[row][col].set(0)
@@ -52,12 +52,12 @@ class Grid:
     def draw(self):
         # Draw Grid Lines
         gap = self.width / 9
-        for i in range(self.rows+1):
+        for i in range(self.rows + 1):
             if i % 3 == 0 and i != 0:
                 thick = 4
             else:
                 thick = 1
-            pygame.draw.line(self.win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (0, i * gap), (self.width, i * gap), thick)
             pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
 
         # Draw Cubes
@@ -88,7 +88,7 @@ class Grid:
             gap = self.width / 9
             x = pos[0] // gap
             y = pos[1] // gap
-            return (int(y),int(x))
+            return (int(y), int(x))
         else:
             return None
 
@@ -167,14 +167,14 @@ class Cube:
         y = self.row * gap
 
         if self.temp != 0 and self.value == 0:
-            text = fnt.render(str(self.temp), 1, (128,128,128))
-            win.blit(text, (x+5, y+5))
-        elif not(self.value == 0):
+            text = fnt.render(str(self.temp), 1, (128, 128, 128))
+            win.blit(text, (x + 5, y + 5))
+        elif not (self.value == 0):
             text = fnt.render(str(self.value), 1, (0, 0, 0))
-            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+            win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
 
         if self.selected:
-            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
 
     def draw_change(self, win, g=True):
         fnt = pygame.font.SysFont("comicsans", 40)
@@ -223,19 +223,19 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
                 return False
 
     return True
 
 
 def redraw_window(win, board, time, strikes):
-    win.fill((255,255,255))
+    win.fill((255, 255, 255))
     # Draw time
     fnt = pygame.font.SysFont("comicsans", 40)
-    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+    text = fnt.render("Time: " + format_time(time), 1, (0, 0, 0))
     win.blit(text, (540 - 160, 560))
     # Draw Strikes
     text = fnt.render("X " * strikes, 1, (255, 0, 0))
@@ -244,29 +244,50 @@ def redraw_window(win, board, time, strikes):
     board.draw()
 
 
+def drawButton(buttonPos, width, height, reset, win, onButton):
+    image = pygame.Surface([width, height])
+    rect = image.get_rect()
+    rect.topleft = buttonPos
+    font = pygame.font.SysFont("comicsans", int(width // 3))
+    image.fill([0, 0, 0])
+    #win.blit(image, buttonPos)
+    text = font.render(reset, False, [255, 0, 0] if onButton else [0, 0, 0])
+    win.blit(text, buttonPos)
+
+
+def ifOnButton(mouse, pos, width, height):
+    if mouse[0] >= pos[0] and mouse[0] <= width and mouse[1] >= pos[1] and mouse[1] <= pos[1] + height:
+        return True
+    return False
+
 def format_time(secs):
-    sec = secs%60
-    minute = secs//60
-    hour = minute//60
+    sec = secs % 60
+    minute = secs // 60
+    hour = minute // 60
 
     mat = " " + str(minute) + ":" + str(sec)
     return mat
 
 
 def main():
-    win = pygame.display.set_mode((540,600))
+    win = pygame.display.set_mode((540, 600))
     pygame.display.set_caption("Sudoku (Press Space to auto solve) ")
     board = Grid(9, 9, 540, 540, win)
     key = None
     run = True
+    #buttons characteristics
+    width = 130
+    height = 30
+    reset = "Reset"
+    buttonPos = [50, 555]
     start = time.time()
     strikes = 0
     while run:
 
         play_time = round(time.time() - start)
-    
+
         for event in pygame.event.get():
-            
+
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
@@ -317,8 +338,11 @@ def main():
 
         if board.selected and key != None:
             board.sketch(key)
-            
+        #print(pygame.mouse.get_pos())
+
         redraw_window(win, board, play_time, strikes)
+        onButton = ifOnButton(pygame.mouse.get_pos(), buttonPos, width, height)
+        drawButton(buttonPos, width, height, reset, win, onButton)
         pygame.display.update()
 
 
